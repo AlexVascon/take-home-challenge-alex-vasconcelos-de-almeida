94f1a031accd97abeffccb93174164e8327889ec
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2022-01-22 16:59:19 +0100

    fix: page loads until all data is present

--- src/components/RefList.tsx
+++ src/components/RefList.tsx
@@ -11,11 +11,11 @@ const RefList: FC<RefListProps> = ({title, list, onClickItem}) => {
     return <div className="row">
         <h3>{title}:</h3>
         <ul className="list-group">
-            {list.map(member => {
+            {list ? list.map(member => {
                 return <li key={member} className="list-group-item">
                     <RefItem url={member} onClick={() => onClickItem && onClickItem(member)} />
                 </li>
-            })}
+            }) : <p>Loading..</p>}
         </ul>
     </div>
 }
