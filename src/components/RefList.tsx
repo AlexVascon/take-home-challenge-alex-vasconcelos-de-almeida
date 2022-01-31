c98a632236eee06da91b1afd72b7c7a8b6145c7e
Alex Vasconcelos de Almeida <1vasconalex1@gmail.com>
2022-01-31 04:37:57 +0100

    refactor: remove unused file as logic had been updated and moved

--- src/components/RefList.tsx
+++ src/components/RefList.tsx
@@ -1,22 +1,49 @@
-import React, { FC } from 'react'
-import RefItem from './RefItem'
+import React, { FC, useEffect, useState } from 'react'
+import CircularProgress from '@mui/material/CircularProgress'
+import { Character, sortedList } from '../service/hero.service'
 
 interface RefListProps {
     list: string[]
     title: string
     onClickItem?: (...values: any[]) => void // TODO MAYBE TYPE THIS BETTER
+    toggleGender?: boolean
 }
 
-const RefList: FC<RefListProps> = ({title, list, onClickItem}) => {
+const RefList: FC<RefListProps> = ({title, list, onClickItem, toggleGender}) => {
+   const [members, setMembers] = useState<Character[] | null>(null)
+   const [membersCopy, setMembersCopy] = useState<Character[] | null>(null)
+
+   useEffect(() => {
+    const fetchMembers = async () => {
+        const characterObjects = await sortedList(list)
+        if(characterObjects) {
+            setMembers(characterObjects)
+            setMembersCopy(characterObjects)
+        } 
+       }
+       fetchMembers()
+   }, [list])
+
+   useEffect(() => {
+    const toggleMaleOnly = async () => {
+        if(toggleGender) {
+            const maleCharacters = membersCopy!.filter(obj => obj.gender === 'Male')
+            setMembers(maleCharacters)
+        } else {
+            setMembers(membersCopy)
+        } 
+    }
+    toggleMaleOnly()
+   }, [membersCopy, toggleGender])
    
     return <div className="row">
         <h3>{title}:</h3>
         <ul className="list-group">
-            {list ? list.map(member => {
-                return <li key={member} className="list-group-item">
-                    <RefItem url={member} onClick={() => onClickItem && onClickItem(member)} />
+            {members ? members.map(member => {
+                return <li key={member.url} className="list-group-item">
+                    <span role='button' onClick={() => onClickItem && onClickItem(member.url)} >{member.name}</span>
                 </li>
-            }) : <p>Loading..</p>}
+            }) : <CircularProgress />}
         </ul>
     </div>
 }
